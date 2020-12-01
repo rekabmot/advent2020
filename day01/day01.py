a7b7a55225b3e7b1aafb3a7f4b089faa0011b452
Tom Baker <tom.baker@mottmac.com>
2020-12-01 08:55:12 +0000

    Day 1

---
+++ day01/day01.py
@@ -0,0 +1,12 @@
+
+filename = "day01/input"
+
+with open(filename) as file_object:
+    lines = list(map(lambda x: int(x), file_object.readlines()))
+
+for x in lines:
+    for y in lines:
+        for z in lines:
+            if x + y + z == 2020:
+                print(x * y * z)
+
