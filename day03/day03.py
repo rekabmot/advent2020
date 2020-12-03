acb713b675538e8f5592595b7fd5651adcba9bdb
Tom Baker <tom.baker@mottmac.com>
2020-12-03 09:09:31 +0000

    Day 03

---
+++ day03/day03.py
@@ -0,0 +1,24 @@
+import functools
+
+filename = "day03/input"
+
+with open(filename) as file_object:
+    grid = [x.strip() for x in file_object.readlines()]
+
+def test(t):
+    pos = (0, 0)
+
+    trees = 0
+
+    while pos[1] < len(grid):
+        if grid[pos[1]][pos[0]] == '#':
+            trees = trees + 1
+
+        pos = ((pos[0] + t[0]) % len(grid[0]), pos[1] + t[1])
+
+    return trees
+
+trajectories = [(1,1),(3,1),(5,1),(7,1),(1,2)]
+
+print(functools.reduce(lambda a, x: a * test(x), [(3, 1)], 1))
+print(functools.reduce(lambda a, x: a * test(x), trajectories, 1))
